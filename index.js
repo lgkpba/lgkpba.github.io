48863b3c147f9cf654fd9729cc04cf49566273f8
lgkpba <lgkpba@gmail.com>
2026-02-25 02:22:49 -0300

    testando mais coisas :O

--- index.js
+++
@@ -1,9 +0,0 @@
-const heatmap = document.querySelector(".heatmap");
-
-for (let i = 0; i < 365; i++) {
-  const day = document.createElement("div");
-  day.classList.add("day");
-  const intensidade = Math.floor(Math.random() * 5);
-  day.classList.add("level-" + intensidade);
-  heatmap.appendChild(day);
-}
