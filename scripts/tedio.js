81ac575938f1420c3636a53028ad3baf345947ea
lgkpba <lgkpba@gmail.com>
2026-03-01 15:38:06 -0300

    acho que entendi pq javascript tem java no nome

--- scripts/tedio.js
+++ scripts/tedio.js
@@ -6,9 +6,47 @@ const larguraTabuleiro = colunas * tamanhoBloco;
 const alturaTabuleiro = linhas * tamanhoBloco;
 let contexto;
 
+const mapa = [
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
+]
+
 window.onload = function() {
     tabuleiro = document.getElementById("tabuleiro");
     tabuleiro.height = alturaTabuleiro;
     tabuleiro.width = larguraTabuleiro;
     contexto = tabuleiro.getContext("2d");
+
+    carregarImagens();
+}
+
+function carregarImagens() {
+
+    bloco = new Image();
+    bloco.src = "../sprites/bloco.png"
+}
+
+class entidade {
+
+    constructor(imagem, x, y, altura, largura) {
+
+        this.imagem = imagem;
+        this.x = x;
+        this.y = y;
+        this.altura = altura;
+        this.largura = largura;
+    }
 }
