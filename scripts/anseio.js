c40924a620238cbaff7ed0022621353aa55c7830
lgkpba <lgkpba@gmail.com>
2026-03-24 13:14:21 -0300

    algum dia eu criptografar a senha :)

--- scripts/anseio.js
+++ scripts/anseio.js
@@ -1,10 +1,10 @@
 //se vc está vendo isto, vc está acabando com a graça. n entre aqui.
 function verificaSenha(){
     var texto = document.getElementById("senha");
-    var tentativa = texto.value;
+    var tentativa = parseInt(texto.value);
     texto.value = ""
 
-    if (tentativa == "teste"){
+    if (Number(tentativa).toString(2) == "10111010100001100011001"){
         window.alert("Algum dia algo vai acontecer aqui")
     }
 }
